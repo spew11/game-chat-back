945cee77f0e972b08e1e523c9cd78cebd09f5cb7
spew11 <dhbdg11@gmail.com>
2023-10-03 01:05:37 +0900

    Feat: typeorm 연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [UsersModule],
+  
+  imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
+    UsersModule
+  ],
 })
 export class AppModule {}
