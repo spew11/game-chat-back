4caaba7d9ba1864b2a4675490f7e901d0888a8ad
eunjilee <dhbdg11@gmail.com>
2023-10-30 21:35:04 +0900

    Fix: userRedirect핸들러에서 cookie헤더 추가 방식 수정, userAdd핸들러에서 유저 로그인 로직 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -31,11 +31,12 @@ export class AuthController {
       this.authService.loginUser(userEmail, req.session);
       res.send({ redirect: 'home' });
     } else {
-      res.cookie('access_token', accessToken, {
-        httpOnly: true,
-        // sameSite: 'strict',
-        maxAge: 720000,
-      });
+      // res.cookie('access_token', accessToken, {
+      //   httpOnly: true,
+      //   // sameSite: 'strict',
+      //   maxAge: 720000,
+      // });
+      res.header('Set-Cookie', [`access_token=${accessToken}; SameSite=None; Secure; Max-Age=720000; HttpOnly=false`]);
       res.send({ redirect: 'register' });
     }
   }
@@ -44,8 +45,10 @@ export class AuthController {
   async userAdd(@Req() req: Request, @Body() createUserDto: CreateUserDto): Promise<void> {
     const accessToken = req.cookies['access_token'];
     if (accessToken) {
-      createUserDto.email = await this.authService.getEmail(accessToken);
+      const userEmail = await this.authService.getEmail(accessToken);
+      createUserDto.email = userEmail;
       this.usersService.createUser(createUserDto);
+      req.session.email = userEmail;
     } else {
       throw new UnauthorizedException('로그인이 필요합니다.');
     }
