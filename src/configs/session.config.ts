b1a6f36564472a89dae2744eed18e50a98f205e4
spew11 <dhbdg11@gmail.com>
2023-10-31 18:04:13 +0900

    Fix: memoryStore -> redis로 변경

--- src/configs/session.config.ts
+++ src/configs/session.config.ts
@@ -1,20 +1,26 @@
 import { ConfigService } from '@nestjs/config';
 
+const session = require('express-session');
+const RedisStore = require('connect-redis').default;
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+  host: 'redis',
+  port: 6379,
+});
+
 declare module 'express-session' {
   interface SessionData {
     email: string;
   }
 }
 
-const session = require('express-session');
-const MemoryStore = require('memorystore')(session);
-
 export function sessionMiddleware(configService: ConfigService) {
   return session({
     secret: configService.get<string>('SESSION_SECRET'),
     resave: true, // 세션 갱신
     saveUninitialized: false,
-    store: new MemoryStore(),
+    store: new RedisStore({ client: redisClient }),
     cookie: {
       maxAge: 30 * 60 * 1000,
       httpOnly: true,
