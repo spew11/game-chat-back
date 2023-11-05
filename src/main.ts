2a4e7f2340722399f428511c5c5f92a94091752b
spew11 <dhbdg11@gmail.com>
2023-11-05 17:01:39 +0900

    Pack: cookie-parser 패키지 추가, cookie-parser 세팅, cookie secure option =true로 변경

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TestService } from './test.service';
-// import cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
 import { sessionMiddleware } from '@configs/session.config';
 import { ValidationPipe } from '@nestjs/common';
@@ -9,9 +9,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  
+
   app.useGlobalPipes(new ValidationPipe());
-  app.use(sessionMiddleware(configService));
+  app.use(sessionMiddleware(configService), cookieParser());
 
   app.enableCors({
     origin: ['http://localhost:3000', 'https://develop.d35lpok7005dz1.amplifyapp.com'], // 요청을 보내는 클라이언트의 주소를 명시
