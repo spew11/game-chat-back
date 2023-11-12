3af8e4af84a4ef402588fad57739cfb13c0dc250
eunjilee <dhbdg11@gmail.com>
2023-11-12 17:37:02 +0900

    Bug: createBlockRelation에서 옵셔널체이닝 잘못 사용으로 버그 발생함-> 삭제함

--- src/user-relation/user-relation.service.ts
+++ src/user-relation/user-relation.service.ts
@@ -110,9 +110,8 @@ export class UserRelationService {
   // user가 otherUser 차단(친구관계여도 무조건 차단함)
   async createBlockRelation(user: User, otherUser: User): Promise<void> {
     const userSide = await this.findUserRelation(user.id, otherUser.id);
-
     // 이미 userSide 관계가 존재한다면 status=block으로 변경
-    if (userSide?.status != UserRelationStatusEnum.BLOCKED) {
+    if (userSide && userSide.status != UserRelationStatusEnum.BLOCKED) {
       userSide.status = UserRelationStatusEnum.BLOCKED;
       this.userRelationRepository.save(userSide);
     } else {
