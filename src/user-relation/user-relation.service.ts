f5a5fe5655b021aaed9ae8f5a1dbe49a421a27ad
eunjilee <dhbdg11@gmail.com>
2023-11-12 16:53:33 +0900

    Bug: findUserRelation을 호출할 때 await 누락된 것 추가

--- src/user-relation/user-relation.service.ts
+++ src/user-relation/user-relation.service.ts
@@ -85,8 +85,8 @@ export class UserRelationService {
   // user-otherUser(status=friend_request)객체 1개, otherUser-user(status=pending_approval) 객체 1개, 총 2개의 객체 생성
   async createFriendRequest(requester: User, recipient: User): Promise<void> {
     // 내가 상대방과 아무 사이아닐 때 가능함. 상대방이 나를 차단했어도 요청은 할 수 있음(상대입장은 여전히 차단함)
-    const requesterSide = this.findUserRelation(requester.id, recipient.id);
-    const recipientSide = this.findUserRelation(recipient.id, requester.id);
+    const requesterSide = await this.findUserRelation(requester.id, recipient.id);
+    const recipientSide = await this.findUserRelation(recipient.id, requester.id);
     if (!requesterSide) {
       // user-otherUser 인스턴스 생성
       const requesterDto = new CreateUserRelationDto();
