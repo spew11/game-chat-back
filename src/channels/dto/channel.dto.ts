5f2471abb0f84a9a1e862697bac90390e9018fc0
ffdfdss <sbsn10@gmail.com>
2023-11-18 15:42:57 +0900

    Fix: DTO 수정

--- src/channels/dto/channel.dto.ts
+++ src/channels/dto/channel.dto.ts
@@ -1,4 +1,4 @@
-import { IsEnum, IsString, MaxLength, MinLength, IsNotEmpty, ValidateIf } from 'class-validator';
+import { IsEnum, IsString, MaxLength, IsNotEmpty, ValidateIf } from 'class-validator';
 import { ChannelType } from '../entities/channel.entity';
 
 export class ChannelDto {
@@ -9,8 +9,6 @@ export class ChannelDto {
 
   @IsString()
   @ValidateIf(o => o.type === ChannelType.protected) // 오직 protected일 때만 적용
-  @IsNotEmpty()
-  @MinLength(4)
   password?: string; // ? 이것으로 public private 채널에서 비밀번호를 필요 없게 만든다
 
   @IsEnum(ChannelType)
