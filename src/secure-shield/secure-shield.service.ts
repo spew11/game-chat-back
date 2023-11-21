004dc84cad10ecc56244a48af7805b58c6870d68
eunjilee <dhbdg11@gmail.com>
2023-11-21 17:10:28 +0900

    Bug: secure-shield.service encrypt, decrypt 메서드 버그 수정

--- src/secure-shield/secure-shield.service.ts
+++ src/secure-shield/secure-shield.service.ts
@@ -27,27 +27,28 @@ export class SecureShieldService {
   }
 
   encrypt(text: string): string {
-    const iv = randomBytes(16); // 초기화 벡터: 같은 데이터를 암호화해도 암호화 결과를 매번 다르게 해준다.
+    const iv = randomBytes(16); // 초기화 벡터: 같은 데이터를 암호화해도 암호화 결과를 매번 다르게 해준다. 16자리 여야 함.
+    // 암호화를 해주는 싸이퍼 객체를 생성함
     const cipher = createCipheriv(
       this.algorithm,
-      Buffer.from(this.configService.get<string>('ENCRYPT_KEY')),
+      this.configService.get<string>('ENCRYPT_KEY'), // AES-256이기 때문에 256비트인 32자리 수의 secret key를 사용해야 함
       iv,
     );
     let encrypted = cipher.update(text); // cipher를 가지고 text를 암호화
     // 암호화된 데이터('encrypted')와 마지막 암호화된 데이터조각 'cipher.final()'을 결합하여 암호화된 데이터를 연속된 버퍼로만듦.
     encrypted = Buffer.concat([encrypted, cipher.final()]);
-    // 바이너리 코드를 16진수로 변환해서 데이터를 쉽게 처리(저장, 전송, 읽기 등)하기 위함
     // 추후에 암호문 해독을 위해서는 iv를 가지고 있어야하기 때문에 암호문과 iv를 같이 반환함.
-    return JSON.stringify({ iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') });
+    // buffer객체는 stringify로 자동으로 직렬화되지 않기 때문에 16진수로 변환해주어야 함
+    return JSON.stringify({ iv: iv.toString('hex'), encrypted: encrypted.toString('hex') });
   }
 
   decrypt(text: string) {
-    const encryptionRes = JSON.parse(text) as { iv: string; encryptedData: string };
-    const iv = Buffer.from(encryptionRes.iv);
-    const encrypted = Buffer.from(encryptionRes.encryptedData);
+    const encryptionRes = JSON.parse(text) as { iv: string; encrypted: string };
+    const iv = Buffer.from(encryptionRes.iv, 'hex'); // JSON.stringfy 때문에 직렬화된 iv를 다시 Buffer타입으로 변환
+    const encrypted = Buffer.from(encryptionRes.encrypted, 'hex');
     const decipher = createDecipheriv(
       this.algorithm,
-      Buffer.from(this.configService.get<string>('ENCRYPT_KEY')),
+      this.configService.get<string>('ENCRYPT_KEY'),
       iv,
     );
     let decrypted = decipher.update(encrypted);
