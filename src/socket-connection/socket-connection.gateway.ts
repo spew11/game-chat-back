742b5281f3c14fbebf5ed7bac4c3b297e7354a30
woojeong <woojin5555@gmail.com>
2023-11-30 11:28:50 +0900

    Fix: main-listening => socket-connection변경

--- src/socket-connection/socket-connection.gateway.ts
+++ src/socket-connection/socket-connection.gateway.ts
@@ -3,8 +3,8 @@ import { Server, Socket } from 'socket.io';
 import { sessionMiddleware } from '@configs/session.config';
 import { ConfigService } from '@nestjs/config';
 import { UseFilters } from '@nestjs/common';
-import { RedisService } from '../../commons/redis-client.service';
-import { WebsocketExceptionsFilter } from '../../filters/websocket-exception.fileter';
+import { RedisService } from '../commons/redis-client.service';
+import { WebsocketExceptionsFilter } from '../filters/websocket-exception.fileter';
 import { corsConfig } from '@configs/cors.config';
 import { RedisField } from 'src/commons/enums/redis.enum';
 
@@ -12,7 +12,7 @@ import { RedisField } from 'src/commons/enums/redis.enum';
 @WebSocketGateway({
   cors: corsConfig,
 })
-export class MainListeningGateway {
+export class SocketConnectionGateway {
   @WebSocketServer() private server: Server;
 
   constructor(
@@ -27,20 +27,17 @@ export class MainListeningGateway {
 
   // client가 연결됬을 때
   async handleConnection(clientSocket: Socket): Promise<void> {
-    const userId = await this.redisService.getUserIdBySession(clientSocket);
+    const userId = await this.getUserIdBySession(clientSocket);
     if (!userId) {
       clientSocket.disconnect(true);
       return;
     }
 
     // 이미 로그인한 유저가 존재할 때 상대 연결 끊기
-    const currentLoginSocketId = await this.redisService.userToSocket(userId);
-    if (currentLoginSocketId) {
-      const currentLoginSocket = this.server.sockets.sockets.get(currentLoginSocketId);
-      if (currentLoginSocket) {
-        currentLoginSocket.disconnect(true);
-        // 상대 session만료시키기?
-      }
+    const currentLoginSocket = await this.userToSocket(userId);
+    if (currentLoginSocket) {
+      currentLoginSocket.disconnect(true);
+      // 상대 session만료시키기?
     }
 
     clientSocket.join(userId.toString());
@@ -49,7 +46,7 @@ export class MainListeningGateway {
 
   // client와 연결이 해제 됬을 때
   async handleDisconnect(clientSocket: Socket): Promise<void> {
-    const userId = await this.redisService.socketToUser(clientSocket.id);
+    const userId = await this.socketToUserId(clientSocket.id);
     if (userId) this.removeClientRedis(clientSocket.id, userId);
   }
 
@@ -63,5 +60,26 @@ export class MainListeningGateway {
     await this.redisService.hdel(clientSocketId, RedisField.SOCKET_TO_USER);
     await this.redisService.hdel(userId, RedisField.USER_TO_SOCKER);
     await this.redisService.hdel(userId, RedisField.USER_STATUS);
+  }
+
+  async socketToUserId(socketId: string): Promise<number> {
+    const userId = parseInt(await this.redisService.hget(socketId, RedisField.SOCKET_TO_USER));
+    return userId;
+  }
+
+  async userToSocket(userId: number): Promise<Socket | null> {
+    const socketId = await this.redisService.hget(userId, RedisField.USER_TO_SOCKER);
+    if (!socketId) return null;
+    const socket = this.server.sockets.sockets.get(socketId);
+    return socket;
+  }
+
+  async getUserIdBySession(clientSocket: Socket): Promise<number | undefined> {
+    let session = clientSocket.request.session;
+    if (!session.userId) {
+      const sessionId = clientSocket.request.headers.authorization;
+      session = JSON.parse(await this.redisService.client.get('session:' + sessionId));
+    }
+    return session?.userId;
   }
 }
---
+++ src/socket-connection/socket-connection.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { SocketConnectionGateway } from './socket-connection.gateway';
+import { CommonsModule } from 'src/commons/commons.module';
+
+@Module({
+  imports: [CommonsModule],
+  providers: [SocketConnectionGateway],
+})
+export class SocketConnectionModule {}
