0a72b36022fa1d1cfba1d39e7e1c4c141be05ab8
woojeong <woojin5555@gmail.com>
2023-11-30 11:28:03 +0900

    Feat: emit, listening => gateway 통합

--- src/notifications/notifications.gateway.ts
+++ src/notifications/notifications.gateway.ts
@@ -1,19 +1,14 @@
-import {
-  ConnectedSocket,
-  SubscribeMessage,
-  WebSocketGateway,
-  WebSocketServer,
-} from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
+import { ConnectedSocket, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 import { NotFoundException, UseFilters } from '@nestjs/common';
-import { WebsocketExceptionsFilter } from '../../filters/websocket-exception.fileter';
+import { WebsocketExceptionsFilter } from '../filters/websocket-exception.fileter';
 import { corsConfig } from '@configs/cors.config';
 import { dtoSerializer } from 'src/utils/dtoSerializer.util';
 import { NotiFriendRequestDto } from 'src/notifications/dtos/noti-friend-request.dto';
 import { NotiChannelInviteDto } from 'src/notifications/dtos/noti-channel-invite.dto';
 import { ChannelsService } from 'src/channels/channels.service';
 import { UserRelationService } from 'src/user-relation/user-relation.service';
-import { RedisService } from 'src/commons/redis-client.service';
+import { SocketConnectionGateway } from 'src/socket-connection/socket-connection.gateway';
 
 type Noti = NotiChannelInviteDto | NotiFriendRequestDto;
 
@@ -21,18 +16,16 @@ type Noti = NotiChannelInviteDto | NotiFriendRequestDto;
 @WebSocketGateway({
   cors: corsConfig,
 })
-export class NotificationListeningGateway {
-  @WebSocketServer() private server: Server;
-
+export class NotificaionsGateway {
   constructor(
-    private redisService: RedisService,
+    private socketConnectionGateway: SocketConnectionGateway,
     private channelsService: ChannelsService,
     private userRelationService: UserRelationService,
   ) {}
 
   @SubscribeMessage('noti-unread')
   async getUnreadNoti(@ConnectedSocket() clientSocket: Socket) {
-    const userId = await this.redisService.socketToUser(clientSocket.id);
+    const userId = await this.socketConnectionGateway.socketToUserId(clientSocket.id);
     if (!userId) {
       throw new NotFoundException('해당 socket으로 userId를 찾을 수 없습니다.: getUnreadNoti');
     }
