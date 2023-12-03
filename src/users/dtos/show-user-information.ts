66d6f57198d0b75be53c443d28810ac9af4c5b69
eunjilee <dhbdg11@gmail.com>
2023-12-03 18:11:33 +0900

    Refactor: 담당 모듈에 serialize 적용

--- src/users/dtos/show-user-information.ts
+++ src/users/dtos/show-user-information.ts
@@ -1,12 +1,20 @@
+import { Expose } from 'class-transformer';
 import { AvatarEnum } from '../enums/avatar.enum';
 
 export class ShowUserInforamtionDto {
+  @Expose()
   id: number;
+  @Expose()
   email: string;
+  @Expose()
   nickname: string;
+  @Expose()
   ladderPoint: number;
+  @Expose()
   avatar: AvatarEnum;
+  @Expose()
   bio: string;
+  @Expose()
   is2fa: boolean;
   // 전적 정보 추가
 }
