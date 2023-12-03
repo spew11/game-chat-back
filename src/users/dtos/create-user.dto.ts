b3dc75147dbc26e143092ed3ab245f4f7c1b1cac
eunjilee <dhbdg11@gmail.com>
2023-12-03 15:58:21 +0900

    담당 모듈에 class-valiidation pipe 추가

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,3 +1,8 @@
+import { IsString, MaxLength, Matches } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @MaxLength(8, { message: '닉네임은 8자리 이하의 영문자와 숫자로만 가능합니다.' })
+  @Matches(/^[a-zA-Z0-9]*$/, { message: '닉네임은 8자리 이하의 영문자와 숫자로만 가능합니다.' })
   nickname: string;
 }
--- src/users/dtos/update-user.dto.ts
+++ src/users/dtos/update-user.dto.ts
@@ -1,7 +1,13 @@
+import { IsEnum, IsString, Matches, MaxLength } from 'class-validator';
 import { AvatarEnum } from '../enums/avatar.enum';
 
 export class UpdateUserDto {
+  @IsString()
+  @MaxLength(8, { message: '닉네임은 8자리 이하의 영문자와 숫자로만 가능합니다.' })
+  @Matches(/^[a-zA-Z0-9]*$/, { message: '닉네임은 8자리 이하의 영문자와 숫자로만 가능합니다.' })
   nickname: string;
+  @IsEnum(AvatarEnum, { message: '아바타는 0부터 5 사이의 숫자여야 합니다.' })
   avatar: AvatarEnum;
+  @MaxLength(60, { message: '자기소개는 최대 60자까지 가능합니다.' })
   bio: string;
 }
