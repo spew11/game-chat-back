3c3c77e79c72a44bc3be42ea71b876491c75297f
eunjilee <dhbdg11@gmail.com>
2023-12-03 16:16:14 +0900

    Fix: validation pipe 옵션 수정

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,8 +1,9 @@
-import { IsString, MaxLength, Matches } from 'class-validator';
+import { IsString, Matches } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
-  @MaxLength(8, { message: '닉네임은 8자리 이하의 영문자와 숫자로만 가능합니다.' })
-  @Matches(/^[a-zA-Z0-9]*$/, { message: '닉네임은 8자리 이하의 영문자와 숫자로만 가능합니다.' })
+  @Matches(/^[\da-zA-Z가-힣]{1,8}$/, {
+    message: '닉네임은 8자리 이하의 한글, 영문자, 숫자로만 가능합니다.',
+  })
   nickname: string;
 }
--- src/users/dtos/update-user.dto.ts
+++ src/users/dtos/update-user.dto.ts
@@ -3,11 +3,13 @@ import { AvatarEnum } from '../enums/avatar.enum';
 
 export class UpdateUserDto {
   @IsString()
-  @MaxLength(8, { message: '닉네임은 8자리 이하의 영문자와 숫자로만 가능합니다.' })
-  @Matches(/^[a-zA-Z0-9]*$/, { message: '닉네임은 8자리 이하의 영문자와 숫자로만 가능합니다.' })
+  @Matches(/^[\da-zA-Z가-힣]{1,8}$/, {
+    message: '닉네임은 8자리 이하의 한글, 영문자, 숫자로만 가능합니다.',
+  })
   nickname: string;
   @IsEnum(AvatarEnum, { message: '아바타는 0부터 5 사이의 숫자여야 합니다.' })
   avatar: AvatarEnum;
-  @MaxLength(60, { message: '자기소개는 최대 60자까지 가능합니다.' })
+  @IsString()
+  @MaxLength(20, { message: '자기소개는 20자 이내만 가능합니다.' })
   bio: string;
 }
